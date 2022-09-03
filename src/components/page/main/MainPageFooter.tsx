82bf7d22aae623f564192f031c1477452f61b429
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-09-04 04:49:04 +0900

    Resolve map key Error

--- src/components/page/main/MainPageFooter.tsx
+++ src/components/page/main/MainPageFooter.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { useAppDispatch } from '../../../redux/hooks';
-import { setUnitData } from '../../../redux/modules/units';
+import { useAppDispatch } from "../../../redux/hooks";
+import { setUnitData } from "../../../redux/modules/units";
 
 const MainPageFooter = () => {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   const clickToStartBattle = () => {
-    navigate("/battle/fdg")
-    dispatch(setUnitData())
-  }
-  
-  return (
-    <MainPageFooterBox><button onClick={clickToStartBattle}>전투 시작</button></MainPageFooterBox>
-  )
-}
+    navigate("/battle/fdg");
+    dispatch(setUnitData());
+  };
 
-const MainPageFooterBox = styled('div')`
+  return (
+    <MainPageFooterBox>
+      <button onClick={clickToStartBattle}>전투 시작</button>
+    </MainPageFooterBox>
+  );
+};
+
+const MainPageFooterBox = styled("div")`
   height: 50px;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #483838;
-`
+`;
 
-export default MainPageFooter
+export default MainPageFooter;

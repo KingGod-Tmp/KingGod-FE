840a9b5b8c3fcf7c13b97729bc4ccd27d0d0a1fa
hysong02 <hysong02@hysong02-macbook.local>
2022-09-04 03:06:00 +0900

    Create MainPage Layout

---
+++ src/components/page/main/MainPageHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainPageHeader = () => {
+  return (
+    <div>전투 메인 페이지</div>
+  )
+}
+
+export default MainPageHeader
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from "styled-components";
+import MainPageBody from "../components/page/main/MainPageBody";
+import MainPageFooter from "../components/page/main/MainPageFooter";
+import MainPageHeader from "../components/page/main/MainPageHeader";
+import { FlexColumnDiv } from "../styles/layouts";
 
 const MainPage = () => {
   return (
-    <div>MainPage</div>
+    <MainPageSection>
+      <MainPageHeader />
+      <MainPageBody />
+      <MainPageFooter />
+    </MainPageSection>
   )
 }
+
+const MainPageSection = styled(FlexColumnDiv)`
+  position: relative;
+  width: inherit;
+  height: inherit;
+  justify-content: space-between;
+`
 
 export default MainPage
